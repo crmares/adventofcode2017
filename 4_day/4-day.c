9c7d22fd9b2994732ebdc4bb7f80efc5e0d5622a
crmares <cristian.mares@stud.acs.upb.ro>
2017-12-11 14:05:45 +0200

    day 4 changes

--- 4_day/4-day.c
+++ 4_day/4-day.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 	char *token = NULL;
 	char **vector;
 	size_t count;
+	int isDuplicate, sum = 0;
 
 	while(getline(&line, &count, f) != -1) {
 		vector = malloc(sizeof(char*) * N);
@@ -32,15 +33,38 @@ int main(int argc, char *argv[])
 		while(token != NULL) {
 			vector[n] = malloc(sizeof(char) * strlen(token));
 			strcpy(vector[n], token);
-			// printf("%s.", vector[n]);
+			char *pch = strstr(vector[n], "\n");
+			if(pch != NULL) {
+  				strncpy(pch, "", 1);
+  				strcpy(vector[n], pch);
+			}
 			n++;
 			token = strtok(NULL, " ");
 		}
+		isDuplicate = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = i + 1; j < n; j++) {
+				if((strcmp(vector[i], vector[j]) == 0) && i != j) {
+					//printf("%s   %s   %d %d\n", vector[i], vector[j], i, j);
+					isDuplicate = 1;
+				}
+			}
+		}
+		if(isDuplicate == 0) {
+			sum++;
+			printf("<<");
+			for(int i = 0; i < n; i++) {
+				printf("%s ", vector[i]);
+			}
+			printf(">>");
+			printf("\n");
+		}
 
-		for(int i = 0; i < n; i++)
-			printf("<<%s>> ", vector[i]);
-		printf("\n");
 	}
+
+	printf("The solution to the puzzle is: %d\n", sum);
+
+	fclose(f);
 
 	return 0;
 }
