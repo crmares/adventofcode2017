24d9f89621e7db60b8181ead8aea49b19149b19c
crmares <cristian.mares@stud.acs.upb.ro>
2017-12-14 17:16:08 +0200

    day 5 uncompleted part 2

---
+++ 5_day/5-day.c
@@ -0,0 +1,61 @@
+//adventofcode day 5
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 2000
+
+int usage() 
+{
+	printf("Usage: <<./a.out>> <<input_file>>  <<'a' for the first part, 'b' for the second one>>\n");
+	exit(0);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc != 3) {
+		usage();
+	}
+
+	FILE *f = fopen(argv[1], "rb");
+	if(f == NULL)
+		usage();
+	int instructions[N];
+	int n = 0, steps, current_step, prev_step, inc;
+
+	while (!feof(f))
+		fscanf(f, "%d", &instructions[n++]);
+
+	steps = 0;
+	current_step = instructions[0];
+	prev_step = current_step;
+	while(1) {
+		if(strcmp(argv[2], "a") == 0) {
+			inc = 1;
+		}
+		else if(strcmp(argv[2], "b") == 0) {
+			if(current_step + instructions[current_step] >= 3)
+				inc = -1;
+			else
+				inc = 1;
+		}
+		else
+			usage();
+
+		prev_step = current_step;
+		current_step += instructions[current_step];
+		instructions[prev_step] += inc;
+		
+		steps++;
+		if(current_step >= n)
+			break;
+	}
+
+	printf("The solution to the puzzle is: %d\n", steps);
+
+	fclose(f);
+
+	return 0;
+}
