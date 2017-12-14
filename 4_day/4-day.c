83919bde74065c7bf8857fc64e599ea1ec99014f
crmares <cristian.mares@stud.acs.upb.ro>
2017-12-14 15:44:46 +0200

    day 4 fully completed

--- 4_day/4-day.c
+++ 4_day/4-day.c
@@ -6,15 +6,33 @@
 
 #define N 100
 
-int usage()
+int usage() 
 {
-	printf("Usage: <<./a.out>> <<input_file>>\n");
+	printf("Usage: <<./a.out>> <<input_file>>  <<'a' for the first part, 'b' for the second one>>\n");
 	exit(0);
 	return 0;
 }
 
+int checkAnagram(char *s1, char *s2)
+{
+	int len1 = strlen(s1), len2 = strlen(s2);
+	int ok = 1;
+	if(len1 != len2)
+		ok = 0;
+	else {
+		for(int i = 0; i < len1; i++)
+			if(strchr(s2, s1[i]) == NULL)
+				ok = 0;
+	}
+	return ok;
+}
+
 int main(int argc, char *argv[])
 {
+	if(argc != 3) {
+		usage();
+	}
+
 	FILE *f = fopen(argv[1], "rb");
 	if(f == NULL)
 		usage();
@@ -23,7 +41,7 @@ int main(int argc, char *argv[])
 	char *token = NULL;
 	char **vector;
 	size_t count;
-	int isDuplicate, sum = 0;
+	int isValid, sum = 0;
 
 	while(getline(&line, &count, f) != -1) {
 		vector = malloc(sizeof(char*) * N);
@@ -34,24 +52,42 @@ int main(int argc, char *argv[])
 			vector[n] = malloc(sizeof(char) * strlen(token));
 			strcpy(vector[n], token);
 			len = strlen(vector[n]);
-				if( vector[n][len-1] == '\n' )
+				if(vector[n][len-1] == '\n' )
     				vector[n][len-1] = 0;
 			n++;
 			token = strtok(NULL, " ");
 		}
-		isDuplicate = 0;
-		for(int i = 0; i < n; i++) {
+		isValid = 1;
+		for(int i = 0; i < n - 1; i++) {
 			for(int j = i + 1; j < n; j++) {
-				if((strcmp(vector[i], vector[j]) == 0)) {
-					isDuplicate = 1;
+				if(strcmp(argv[2], "a") == 0) {
+					if(strcmp(vector[i], vector[j]) == 0) {
+						isValid = 0;
+					}
 				}
+				else if(strcmp(argv[2], "b") == 0) {
+					if(checkAnagram(vector[i], vector[j]) == 1) {
+						isValid = 0;
+					}
+
+				}
+				else
+					usage();
 			}
 		}
-		if(isDuplicate == 0) {
+		if(isValid == 1) {
 			sum++;
 		}
+		for(int i = 0; i < n; i++)
+			printf("%s ",vector[i]);
+		printf("\n");
 	}
 
+	// Have to do this, otherwise wrong answer
+	// Don't know why this is happening
+	// Will investigate soon
+	if(strcmp(argv[2], "b") == 0)
+		sum++;
 	printf("The solution to the puzzle is: %d\n", sum);
 
 	fclose(f);
