cc82e7cdba8720b327c23aec24b13907e9b1eee5
EC2 Default User <ec2-user@ip-172-31-25-134.eu-west-1.compute.internal>
2017-12-19 15:44:50 +0000

    day 7 - having some issues while accessing the tree

--- 7_day/7-day.c
+++ 7_day/7-day.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 300
-#define M 1000
+#define N 100
+#define M 1300
+#define WORD_SIZE 20
+#define COST_SIZE 5
 
 int usage() 
 {
@@ -31,35 +33,74 @@ int main(int argc, char *argv[])
     if(f == NULL)
         usage();
     
-    int len, n = 0;
+    int len, n = 0, m;
     char *line = malloc(sizeof(char) * N);
-    char *token = NULL, *number = malloc(sizeof(char) * 10);
+    char *token = NULL, *number = malloc(sizeof(char) * COST_SIZE);
     char ***tree = (char***)malloc(sizeof(char**) * M);
-    int cost[M];
+    int cost[M], hasChild[M];
     size_t count;
 
+    memset(hasChild, 0, sizeof(hasChild));
+    
+    for(int i = 0; i < M; i++)
+            tree[i] = (char**)malloc(sizeof(char*) * 20);
     
     while(getline(&line, &count, f) != -1) {
+        m = 0;
         len = strlen(line);
 		if(line[len-1] == '\n')
     	    line[len-1] = 0;
     	    
-        for(int i = 0; i < M; i++)
-            tree[i] = (char**)malloc(sizeof(char*) * M);
-    	    
     	token = strtok(line, " ");
-    	tree[n][0] = malloc(sizeof(char) * M);
-    	strcpy(tree[n][0], token);
+    	tree[n][0] = malloc(sizeof(char) * WORD_SIZE);
+    	strcpy(tree[n][m], token);
 		    while(token != NULL) {
 		        if(token[0] == '(') {
 		            strcpy(number, token);
 		            number = getAllButFirstAndLast(number);
-		            printf("%s\n", number);
+		            cost[n] = atoi(number);
+		        }
+		        else if(token[0] == '-') {
+		            //do nothing
+		        }
+		        else {
+		            m++;
+		            tree[n][m] = malloc(sizeof(char) * WORD_SIZE);
+		            strcpy(tree[n][m], token);
+		            if(tree[n][m][strlen(tree[n][m]) - 1] == ',')
+		                tree[n][m][strlen(tree[n][m]) - 1] = '\0';
 		        }
 		        
 		    token = strtok(NULL, " ");
 		    }
+		 printf("%s  %d  ", tree[n][0], cost[n]);
+// 		 for(int i = 1; i < m; i ++)
+// 		    printf("%s  ", tree[n][i]);
+// 		 printf("\n");
+// 		 printf("%d\n", strlen(tree[n]));
+//     tree[n][m + 1] = malloc(sizeof(char) * WORD_SIZE);
+    n++;
     }
+    // printf("%d\n", n);
+    for(int i = 0; i < n; i++) {
+        printf("%s\n", tree[i][0]);
+    }
+
+    // for(int i = 0; i < n; i++) {
+    //     m = 1;
+    //     // if(tree[i][m] == NULL)
+    //     //     printf("dadasdAdada\n");
+    //     while(tree[i][m] != NULL) {
+    //         for(int j = 0; j < n; j++)
+    //             if(strcmp(tree[j][0], tree[i][m]) == 0)
+    //                 hasChild[j] = 1;
+    //     m++;
+    //     }
+    // }
+
+    // for(int i = 0; i < n; i++) 
+    //     if(hasChild[i] == 0)
+    //         printf("%s\n", tree[i][0]);
         
     fclose(f);        
     
